86f6e8b9448d0118934fa76183da2f1ca62f9240
Henrique Bernard Wagner <henriquebernardwagner@gmail.com>
2023-03-23 22:27:30 -0300

    NewCommit

---
+++ App.js
@@ -0,0 +1,60 @@
+import React from "react";
+import {
+  ScrollView,
+  View,
+  Text,
+  Image, 
+  TouchableOpacity,
+  TextInput
+
+} from 'react-native';
+import { styles } from "./styles";
+
+const App = () => {
+  return (
+    <ScrollView style={styles.scrollView}>
+        <View style={styles.container}>
+          <Image source={require('./assets/logo.png')} style={styles.logo}/>
+          <Text style={styles.h1} >Sistema de Login</Text>
+          <Text style={styles.h2}>Bem vindo(a)! Digite seus dados abaixo</Text>
+          
+          <View style={styles.inputArea}>
+            <Text style={styles.inputLabel}>Email</Text>
+            <TextInput 
+            style={styles.inputField}
+            placeholder="Digite seu email"
+            />
+          </View>
+          <View style={styles.inputArea}>
+            <Text style={styles.inputLabel}>Senha</Text>
+            <TextInput 
+            style={styles.inputField}
+            placeholder="*******"
+            secureTextEntry
+            />
+          </View>
+          <View style={styles.aditionals}>
+            <TouchableOpacity style={styles.forgotBtnArea}>
+              <Text style={styles.forgotBtnText}>Esqueci minha senha</Text>
+            </TouchableOpacity>
+
+          </View>
+
+          <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Entrar</Text>
+          </TouchableOpacity>
+         
+
+          <View style={styles.signUpArea}>
+            <Text style={styles.signUpText}>Não tem uma conta? Cadastre-se</Text>
+              
+          </View>
+          <View  style={styles.footerArea}>
+            
+          </View>
+        </View>
+    </ScrollView>
+  );
+}
+
+export default App
---
+++ styles.js
@@ -0,0 +1,91 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+
+  scrollView: {
+    backgroundColor: '#FFF',
+    paddingHorizontal:15,
+    paddingTop:40
+  },
+  container:{
+    alignItems: 'center',
+  },
+  logo:{
+    width:150,
+    height:150
+  },
+  h1:{
+    color:'#000',
+    fontSize:24,
+    fontWeight:'bold',
+    marginVertical:10
+
+  },
+  h2:{
+    color:'#993',
+    fontSize:17
+
+  },
+  inputArea:{
+    width:'100%',
+    paddingTop:20,
+    fontWeight:'bold',
+
+
+  },
+  inputLabel:{
+    color:'#777',
+    fontSize:15,
+    marginBottom:7
+
+  },
+  inputField:{
+    borderWidth:2,
+    borderRadius:6,
+    borderColor:'#DDd',
+    fontSize:15,
+    padding:10
+
+  },
+  aditionals:{},
+  forgotBtnArea:{
+    
+  },
+  forgotBtnText:{
+    color: "#0000ff",
+    fontWeight: "bold",
+    marginLeft:150,
+    marginTop: 10
+  },
+  button:{
+    color: "#0000ff",
+    borderColor:'#0000',
+    fontSize:15,
+    padding:'20px',
+    marginBottom:50,
+    
+    
+  },
+  buttonText:{
+    borderWidth:0,
+    borderRadius:6,
+    borderColor:'#DDd',
+    backgroundColor: '#0000ff',
+    fontSize:15,
+    padding:'15px',
+    marginVertical:20,
+    paddingHorizontal:150
+    
+    
+  },
+  signUpArea:{
+  
+  },
+  signUpText:{},
+  signUpBtnText:{},
+  footerArea:{
+    
+  },
+  footerText:{}
+
+});
